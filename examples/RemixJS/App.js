eebacb019d7ce944c7727fe4d621217627f4dc13
Ajay Sivan <ajayesivan@gmail.com>
2022-04-22 05:46:08 +0530

    Basic setup

--- examples/RemixJS/App.js
+++ examples/RemixJS/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Text} from 'react-native';
+import RemixIcon from 'react-native-remix-icon';
 
 const App = () => {
-  return <Text>Hello!!!</Text>;
+  return <RemixIcon text="Hello" />;
+  // return <Text>Hello</Text>;
 };
 
 export default App;
---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+declare type Props = {
+    text: string;
+};
+declare const RemixIcon: ({ text }: Props) => JSX.Element;
+export default RemixIcon;
