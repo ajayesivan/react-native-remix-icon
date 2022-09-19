74fa6c8c5fa4d11268f10a81e4e80bc9e4efa47c
AntoineKM <etorefr@gmail.com>
2022-09-19 15:45:36 +0000

    Add types

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+declare interface RemixIconProps {
+  name: string;
+  size?: number | string;
+  color?: string;
+  [key: string]: any;
+}
+declare const RemixIcon: React.FC<RemixIconProps>;
+export default RemixIcon;
