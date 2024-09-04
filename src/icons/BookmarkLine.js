db5b4a406a1403371e59768c1a2bd72a30057cd3
Mukhtar Oyelayo <mukhtaroyelayo@gmail.com>
2024-09-04 13:25:18 +0100

    chore: update icons

--- src/icons/BookmarkLine.js
+++ src/icons/BookmarkLine.js
@@ -1,14 +1,13 @@
 import * as React from "react";
 import Svg, { Path } from "react-native-svg";
-const SvgBookmarkLine = props => (
-  <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="currentColor"
-    viewBox="0 0 24 24"
-    {...props}
-  >
-    <Path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1m13 2H6v15.432l6-3.761 6 3.761z" />
-  </Svg>
-);
+
+function SvgBookmarkLine(props) {
+  return (
+    <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+      <Path fill="none" d="M0 0h24v24H0z" />
+      <Path d="M5 2h14a1 1 0 011 1v19.143a.5.5 0 01-.766.424L12 18.03l-7.234 4.536A.5.5 0 014 22.143V3a1 1 0 011-1zm13 2H6v15.432l6-3.761 6 3.761V4z" />
+    </Svg>
+  );
+}
 
 export default SvgBookmarkLine;
